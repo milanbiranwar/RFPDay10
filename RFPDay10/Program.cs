2f6383d8c0a0962f1a5bddef8631680628ed25d4
Milan Biranwar <milanbiranwar06@gmail.com>
2022-05-30 23:34:29 +0530

    program for Address Book of Day10

--- RFPDay10/Program.cs
+++ RFPDay10/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RFPDay10;
+
 Console.WriteLine("Hello, World!");
+
+AddMultipleContact addressBook = new AddMultipleContact();
+
+addressBook.AddMultipleContact1();
+
+addressBook.AddNewContact();
+addressBook.AddNewContact();
+
+AddMultipleContact.PrintAddressBook();
+Console.WriteLine("Enter the FirstName to Edit");
+string firstName = Console.ReadLine();
+addressBook.EditContact(firstName);
+AddMultipleContact.PrintAddressBook();
+
+AddMultipleContact.PrintAddressBook();
+Console.WriteLine("Enter the FirstName to Delete");
+string firstName1 = Console.ReadLine();
+addressBook.DeleteContact(firstName1);
+AddMultipleContact.PrintAddressBook();
+Console.ReadLine();
