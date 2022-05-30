44c9120ea1f3708a15cadf9bad0d03e18e0d4652
Milan Biranwar <milanbiranwar06@gmail.com>
2022-05-30 23:58:45 +0530

    Program for Multiple Address Book

--- RFPDay10/Contact.cs
+++ RFPDay10/Contact.cs
@@ -17,6 +17,7 @@ namespace RFPDay10
         public string ZipCode { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string ContactBook { get; set; }
 
     }
 }
---
+++ RFPDay10/MultipleAddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFPDay10
+{
+    internal class MultipleAddressBook
+    {
+        
+        Dictionary<string, List<Contact>> MultiDict = new Dictionary<string, List<Contact>>();
+        public void DictionaryContacts()
+        {
+            AddressBook ObjAdd = new AddressBook();
+            Console.WriteLine("How many ADDRESSBOOK, You want to create ");
+            int BookNum = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= BookNum; i++)
+            {
+                Console.WriteLine("How many contcts you want to add in Addressbook ");
+                int ContactNum = Convert.ToInt32(Console.ReadLine());
+                for (int j = 1; j <= ContactNum; j++)
+                {
+                    ObjAdd.AddNewContact();
+                }
+                Console.WriteLine("What name you want to give to address book");
+                string Bookname = Console.ReadLine();
+                MultiDict.Add(Bookname, ObjAdd.ContactBook);
+            }
+            Console.WriteLine("AddressBook added successfully");
+        }
+    }
+}
